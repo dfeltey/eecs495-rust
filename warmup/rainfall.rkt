bd5b92d96f03dc5630bf1e24c2b92297f10fec9a
Robby Findler <robby@racket-lang.org>
2016-01-07 07:26:14 -0600

    refactor and add tests

--- warmup/rainfall.rkt
+++ warmup/rainfall.rkt
@@ -1,30 +1,39 @@
 #lang racket
 
+;; -> void
 (define (main)
   (define readings (get-readings (current-input-port)))
   (define mn (mean readings))
   (displayln (~a "Mean rainfall: " mn  " cm" ))
-  (displayln (~a "Below count : " (below-count mn readings)))
-  (displayln (~a "Above count : " (above-count mn readings))))
+  (displayln (~a "Below count : " (count mn readings #t)))
+  (displayln (~a "Above count : " (count mn readings #f))))
 
 ;; mean : (listof nonnegative?) -> nonnegative?
 (define (mean readings)
   [/ [apply + readings] [length readings]])
 
-;; below-count : nonnegative (listof nonnegative?) -> natural
-(define (below-count mean readings)
-  (length (filter (λ (n) (and (> mean n) (>= n (- mean 5)))) readings)))
+;; count : nonnegative (listof nonnegative?) boolean? -> natural
+(define (count mean readings below?)
+  (for/sum ([reading (in-list readings)])
+    (if (include? mean reading below?)
+        1
+        0)))
 
-;; above-count : nonnegative (listof nonnegative?) -> natural
-(define (above-count mean readings)
-  (length (filter (λ (n) (and (>= (+ mean 5) n) (> n mean))) readings)))
+;; include? : real? real? boolean? -> boolean?
+(define (include? mean reading below?)
+  (if below?
+      (and (> mean reading) (<= (- mean 5) reading))
+      (and (< mean reading) (>= (+ mean 5) reading))))
 
 (define (get-readings in)
   (for*/list ([line (in-lines (terminate-on-999 in))]
               [n (in-value (string->number line))]
               #:when (and n (>= n 0)))
     n))
-    
+
+;; terminate-on-999 : input-port -> input-port
+;; result port is copied from input, unless input has a line of 999
+;; in which case the result port closes
 (define (terminate-on-999 p)
   (define-values (in out) (make-pipe))
   (thread
@@ -50,5 +59,14 @@
   (check-equal? (get-readings (open-input-string "three")) '())
   (check-equal? (get-readings (open-input-string "3\n")) '(3))
   (check-equal? (get-readings (open-input-string "3\n999\n4")) '(3))
-  )
-  
+
+  (check-equal? (count 5 (build-list 10 values) #t) 5)
+  (check-equal? (count 5 (build-list 10 values) #f) 4)
+
+  (check-equal?
+   (let ([sp (open-output-string)])
+     (parameterize ([current-input-port (open-input-string "1\n2\n3\n4\n10\n-100")]
+                    [current-output-port sp])
+       (main))
+     (get-output-string sp))
+   "Mean rainfall: 4 cm\nBelow count : 3\nAbove count : 0\n"))
