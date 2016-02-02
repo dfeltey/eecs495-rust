ece361ef4990d6d9a416cbab62e971f80ee61cab
Daniel Feltey <dfeltey@gmail.com>
2016-02-02 09:39:25 -0600

    Minor cleanup changes, start writing tests for graph building

--- hw/graph/src/main.rs
+++ hw/graph/src/main.rs
@@ -6,6 +6,7 @@ use std::fs::File;
 fn main() {
 }
 
+#[derive(PartialEq,Debug)]
 struct Graph {
     edges: HashMap<String,HashSet<String>>
 }
@@ -29,6 +30,7 @@ impl Graph {
             let line = line_result.expect("Failed to read");
             let mut words = line.split_whitespace();
             let vertex = words.next().expect("No Vertex to read");
+            graph.entry(vertex.to_owned()).or_insert(HashSet::new());
             if seen.contains(vertex) {
                 panic!("A vertex appears on multiple lines")
             }
@@ -38,7 +40,7 @@ impl Graph {
             neighbors_not_seen.remove(vertex);
             for edge in words {
                 neighbors_not_seen.insert(edge.to_owned());
-                graph.entry(vertex.to_owned()).or_insert(HashSet::new()).insert(edge.to_owned());
+                graph.get_mut(vertex).expect("expected vertex is missing").insert(edge.to_owned());
                 graph.entry(edge.to_owned()).or_insert(HashSet::new()).insert(vertex.to_owned());
             }
         }
@@ -46,5 +48,65 @@ impl Graph {
             panic!("a neighbor of some vertex does not appear on its own line");
         }
         return Graph{edges: graph};
+    }
+}
+
+
+#[cfg(test)]
+mod graph_tests {
+    use super::Graph;
+    use std::io::{BufReader,BufRead,Read};
+    use std::io::Result;
+    use std::collections::{HashMap,HashSet};
+
+
+    #[test]
+    #[should_panic]
+    fn missing_vertex() {
+        Graph::build_graph(StringReader::new("a b\n"));
+    }
+
+    #[test]
+    #[should_panic]
+    fn duplicate_vertices() {
+        Graph::build_graph(StringReader::new("a b c\nb c\nc\na c"));
+    }
+
+    #[test]
+    fn single_vertex() {
+        let mut edges = HashMap::new();
+        edges.insert("a".to_owned(),HashSet::new());
+        let expected = Graph{edges: edges};
+        let actual = Graph::build_graph(StringReader::new("a"));
+        assert_eq!(expected,actual);
+    }
+    
+
+    struct StringReader {
+        contents: Vec<u8>,
+        position: usize,
+    }
+
+    impl StringReader {
+        fn new(s: &str) -> Self {
+            StringReader {
+                contents: s.to_owned().into_bytes(),
+                position: 0,
+            }
+        }
+    }
+
+    impl Read for StringReader {
+        fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+            let mut count = 0;
+            
+            while self.position < self.contents.len() && count < buf.len() {
+                buf[count] = self.contents[self.position];
+                count += 1;
+                self.position += 1;
+            }
+            
+            return Ok(count);
+        }
     }
 }
