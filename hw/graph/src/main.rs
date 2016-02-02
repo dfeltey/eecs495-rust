c1a80991d2b135db2b32016d7377cbd45c786e69
Daniel Feltey <dfeltey@gmail.com>
2016-02-02 09:04:55 -0600

    Clean up {} only used to satisfy the borrow checker and add error handling

--- hw/graph/src/main.rs
+++ hw/graph/src/main.rs
@@ -4,13 +4,6 @@ use std::env;
 use std::fs::File;
 
 fn main() {
-    let mut h: HashMap<u8,u8> = HashMap::new();
-    h.insert(1,2);
-    h.insert(3,4);
-    h.insert(5,6);
-    {let i = h.entry(7).or_insert(12);};
-    {let j = h.entry(1).or_insert(13);};
-  
 }
 
 struct Graph {
@@ -21,28 +14,32 @@ impl Graph {
     fn new(filename: String) -> Self {
         match File::open(filename) {
             Err(_) => panic!("couldn't open file"),
-            Ok(file) => Graph::build_graph_from_file(file),
+            Ok(file) => Graph::build_graph(file),
         }
     }
 
-    fn build_graph_from_file(file: File) -> Self {
+    fn build_graph<R : Read>(reader: R) -> Self {
         let mut graph = HashMap::new();
-        let line_reader = BufReader::new(file);
-        // ...
-        let lines = line_reader.lines();
-        for line_result in lines {
-            let line: String = line_result.expect("Failed to read");
+        // keep track of which nodes have been mentioned as neighbors
+        // to make sure they all start a line as well
+        let mut neighbors_not_seen = HashSet::new();
+        // Ensure each node is seen as the start of only a single line
+        let mut seen = HashSet::new();
+        for line_result in BufReader::new(reader).lines() {
+            let line = line_result.expect("Failed to read");
             let mut words = line.split_whitespace();
             let vertex = words.next().expect("No Vertex to read");
+            if seen.contains(vertex) {
+                panic!("A vertex appears on multiple lines")
+            }
+            else {
+                seen.insert(vertex.to_owned());
+            }
+            neighbors_not_seen.remove(vertex);
             for edge in words {
-                {
-                    let v_edges = graph.entry(vertex.to_owned()).or_insert(HashSet::new());
-                    v_edges.insert(edge.to_owned());
-                };
-                {
-                    let e_edges = graph.entry(edge.to_owned()).or_insert(HashSet::new());
-                    e_edges.insert(vertex.to_owned());
-                };
+                neighbors_not_seen.insert(edge.to_owned());
+                graph.entry(vertex.to_owned()).or_insert(HashSet::new()).insert(edge.to_owned());
+                graph.entry(edge.to_owned()).or_insert(HashSet::new()).insert(vertex.to_owned());
             }
         }
         return Graph{edges: graph};
