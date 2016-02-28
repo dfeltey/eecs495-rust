175034981d6d2d5aaf8dfaef89a8bed962049e5d
Robby Findler <robby@racket-lang.org>
2016-02-28 16:48:55 -0600

    add another example

---
+++ mini-thd/ex2.rkt
@@ -0,0 +1,16 @@
+#lang s-exp "lang.rkt"
+
+(var y 0)
+
+(var l0 (rec (hd 0) (tl false)))
+(var l1 (rec (hd 1) (tl l0)))
+(var l2 (rec (hd 2) (tl l1)))
+(var l3 (rec (hd 3) (tl l2)))
+
+(define (drop-next l)
+  (:= (dot l tl) (dot (dot l tl) tl)))
+
+(seq (par (drop-next l3)
+          (drop-next l2)
+          (drop-next l1))
+     l3)
