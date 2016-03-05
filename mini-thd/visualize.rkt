44600f09568baafaff357afb240462e22afdc0dd
Robby Findler <robby@racket-lang.org>
2016-03-04 18:47:17 -0600

    get rid of football-related stuff

--- mini-thd/visualize.rkt
+++ mini-thd/visualize.rkt
@@ -63,15 +63,6 @@
   (define backwards-neighbors (graph-backwards-neighbors a-graph))
   (hash-set! neighbors n1 (cons n2 (get-neighbors a-graph n1)))
   (hash-set! backwards-neighbors n2 (cons n1 (get-backwards-neighbors a-graph n2))))
-(define (remove-node! a-graph n)
-  (define neighbors (graph-neighbors a-graph))
-  (define backwards-neighbors (graph-backwards-neighbors a-graph))
-  (define preds (get-backwards-neighbors a-graph n))
-  (define succs (get-neighbors a-graph n))
-  (for ([pred (in-list preds)])
-    (hash-set! neighbors pred (remove n (hash-ref neighbors pred))))
-  (for ([succ (in-list succs)])
-    (hash-set! backwards-neighbors succ (remove n (hash-ref backwards-neighbors succ)))))
 (define (add-hb-edge! a-graph n1 n2)
   (define hb (graph-hb a-graph))
   (hash-set! hb n1 (cons n2 (hash-ref hb n1 '()))))
@@ -81,26 +72,6 @@
   (define hb (make-hash))
   (define nodes (make-hash))
   (graph neighbors backwards-neighbors hb nodes))
-(define (make-a-graph connections)
-  (define a-graph (make-empty-graph))
-  (define nodes (for*/set ([connection (in-list connections)]
-                           [node (in-list connection)])
-                  connection))
-  (for ([node (in-list (sort (set->list nodes) string<? #:key ~s))])
-    (new-basic-node g node))
-
-(module+ test
-  (let ()
-    (define g (make-empty-graph))
-    (define n1 (new-basic-node g "1"))
-    (define n2 (new-basic-node g "2"))
-    (define n3 (new-basic-node g "3"))
-    (add-edge! g n1 n2)
-    (add-edge! g n2 n3)
-    (add-edge! g n1 n3)
-    (remove-node! g n2)
-    (check-equal? (graph-neighbors g) (make-hash '(("n1" "n3"))))
-    (check-equal? (graph-backwards-neighbors g) (make-hash '(("n3" "n1"))))))
 
 (define (build-basic-graph transcript)
   (define a-graph (make-empty-graph))
@@ -171,84 +142,3 @@
                              (t-choice '(1) (mksrc 3))
                              (t-choice '#(() join) (mksrc 4)))))
    (make-hash '(("n1" . ("n2")) ("n2" . ("n3")) ("n0" . ("n1")) ("n3" . ("n4"))))))
-
-;; -> (or/c #f (cons string?[head] string?[tail]))
-(define (find-football a-graph)
-  (define nodes (hash-keys (graph-nodes a-graph)))
-  (for/or ([node (in-list nodes)])
-    (define end (find-football-from a-graph node))
-    (and end (cons node end))))
-
-;; find-football-from : graph string -> (or/c #f string)
-(define (find-football-from a-graph node)
-  (define neighbors (get-neighbors a-graph node))
-  (cond
-    [(> (length neighbors) 1)
-     (define come-together-candidates (make-hash))
-     (let loop ([n (car neighbors)])
-       (hash-set! come-together-candidates n #t)
-       (define neighbors (get-neighbors a-graph n))
-       (when (= 1 (length neighbors))
-         (loop (car neighbors))))
-
-     (define (find-come-together-candidate n)
-       (let loop ([n n])
-         (cond
-           [(hash-ref come-together-candidates n #f)
-            n]
-           [else
-            (define neighbors (get-neighbors a-graph n))
-            (cond
-              [(= 1 (length neighbors))
-               (loop (car neighbors))]
-              [else #f])])))
-     
-     (define first-come-together-candidate (find-come-together-candidate (cadr neighbors)))
-     (and first-come-together-candidate
-          (for/and ([neighbor (in-list (cddr neighbors))])
-            (equal? (find-come-together-candidate neighbor)
-                    first-come-together-candidate)))]
-    [else #f]))
-
-(define (direct-path-to a-graph n come-together)
-  (let loop ([n n])
-    (cond
-      [(equal? n come-together) #t]
-      [else
-       (define neighbors (get-neighbors a-graph n))
-       (and (= 1 (length neighbors))
-            (loop (car neighbors)))])))
-
-;(define (replace-football a-graph start end node)
-  
-
-(module+ test
-
-  (define nodes
-    (let ()
-      (define nodes (make-hash))
-      (define g (graph (make-hash) (make-hash) (make-hash) nodes))
-      (new-basic-node g "zero")
-      (new-basic-node g "one")
-      (new-basic-node g "two")
-      (new-basic-node g "three")
-      (new-basic-node g "four")
-      (new-basic-node g "five")
-      nodes))
-  nodes
-
-  (check-equal?
-   (find-football
-    (graph (make-hash '(("n1" . ("n3" "n2")) ("n2" . ("n4")) ("n0" . ("n1")) ("n3" . ("n4"))))
-           (make-hash)
-           nodes))
-   (cons "n1" "n4"))
-
-  (check-equal?
-   (find-football
-    (graph (make-hash '(("n1" . ("n3" "n2" "n5"))
-                        ("n2" . ("n4")) ("n0" . ("n1")) ("n3" . ("n4"))
-                        ("n5" . ("n3"))))
-           (make-hash)
-           nodes))
-   #f))
